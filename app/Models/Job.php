58d6e256704dc6788cd62f284d70f3817b7813c6
Kent Joemar L. Escoto <kentescoto24@gmail.com>
2024-08-14 10:29:26 +0800

    day-12 (Pivot Tables and BelongsToMany Relationships)

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,15 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model {
+class Job extends Model
+{
 
-  use HasFactory;
+    use HasFactory;
 
-  protected $table = 'job_listings';
+    protected $table = 'job_listings';
 
-  protected $fillable = ['title', 'salary'];
+    protected $fillable = ['title', 'salary'];
 
-  public function employer() {
-    return $this->belongsTo(Employer::class);
-  }
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
+    }
 };
